35ab6d0942079b135ee90525ec55744cc0add626
Andelf <andelf@gmail.com>
2023-12-24 13:44:03 +0800

    fix(rcc): init clk as hclk=sysclk=48mhz

--- src/rcc.rs
+++ src/rcc.rs
@@ -1,3 +1,5 @@
+use crate::pac;
+
 use fugit::HertzU32 as Hertz;
 
 const HSI_FREQUENCY: Hertz = Hertz::from_raw(48_000_000);
@@ -17,4 +19,14 @@ pub struct Clocks {
 #[inline]
 pub fn clocks() -> &'static Clocks {
     unsafe { &CLOCKS }
+}
+
+pub(crate) fn init() {
+    let flash = unsafe { &*pac::FLASH::PTR };
+    let rcc = unsafe { &*pac::RCC::PTR };
+
+    flash.actlr.modify(|_, w| w.latency().variant(0b10)); // 2 等待（24MHz<HCLK<=48MHz）
+
+    // set hckl = sysclk = APB1
+    rcc.cfgr0.modify(|_, w| w.hpre().variant(0));
 }
